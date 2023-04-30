616b39923994a670a6218e4713f4166504e1dc9e
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-04-30 12:35:58 +0300

    8. add classNames.ts

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Theme, ThemeContext} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
-
     return (
-        <div className={`app ${theme}`}>
+        // <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <Link to={'/'}>Link to main page</Link>
             <Link to={'/about'}>Link to about page</Link>
             <Suspense fallback={<div>Loading...</div>}>
