7a443084f2f5eb4c15637a17ec521cbe5d856183
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-05-05 00:03:13 +0300

    20. add jest, add tests

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,37 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with mods true', () => {
+        const expected = 'someClass hovered scrollable';
+        expect(classNames('someClass', { hovered: true, scrollable: true }))
+            .toBe(expected);
+    });
+
+    test('with mods falls', () => {
+        const expected = 'someClass scrollable';
+        expect(classNames('someClass', { hovered: false, scrollable: true }))
+            .toBe(expected);
+    });
+
+    test('with mods undefined', () => {
+        const expected = 'someClass scrollable';
+        expect(classNames('someClass', { hovered: undefined, scrollable: true }))
+            .toBe(expected);
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass additional test';
+        expect(classNames('someClass', {}, ['additional', 'test']))
+            .toBe(expected);
+    });
+
+    test('with additional class and mods', () => {
+        const expected = 'someClass additional test hovered scrollable';
+        expect(classNames('someClass', { hovered: true, scrollable: true }, ['additional', 'test']))
+            .toBe(expected);
+    });
+});
