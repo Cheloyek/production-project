c733d28fac9663186b53e2832015a38857179f88
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-10-10 22:30:04 +0300

    add componentRender

--- src/widgets/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/Sidebar.test.tsx
@@ -3,18 +3,18 @@ import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import {
     renderWithTranslation,
 } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('sidebar', () => {
     test('render sidebar', () => {
         // const SidebarWithTranslation = withTranslation()(Sidebar);
         // render(<SidebarWithTranslation />);
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('sidebar toggle', () => {
-        renderWithTranslation(<Sidebar />);
-
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
